2342757357dd38c3721a6f3eeb21e18ffe324eb8
aadil-nv <adilev2000@gmail.com>
2025-09-29 14:17:54 +0530

    initial setup completed configured vite,eslint,tailwind,ts

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-600 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
